36a6e22985c0ff5553f5346f2b06b532af8ae25c
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-03 17:33:58 -0200

    InfoCityMap carregando o layout map.

--- src/br/ufsm/brunodea/tcc/map/InfoCityMap.java
+++ src/br/ufsm/brunodea/tcc/map/InfoCityMap.java
@@ -1,9 +1,19 @@
 package br.ufsm.brunodea.tcc.map;
 
+import android.os.Bundle;
+import br.ufsm.brunodea.tcc.R;
+
 import com.google.android.maps.MapActivity;
 
 public class InfoCityMap extends MapActivity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceBundle) {
+		super.onCreate(savedInstanceBundle);
+		
+		setContentView(R.layout.map);
+	}
+	
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
