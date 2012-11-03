d5d5583a585e304ed22413e53d651305b3dc72e0
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-03 17:17:15 -0200

    projeto inicializado.

---
+++ src/br/ufsm/brunodea/tcc/map/InfoCityMap.java
@@ -0,0 +1,11 @@
+package br.ufsm.brunodea.tcc.map;
+
+import com.google.android.maps.MapActivity;
+
+public class InfoCityMap extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+}
