622370ab234e748359318568edd0d906ac3e3ada
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-05 20:58:51 -0200

    adicionei a classe EventItem.

---
+++ src/br/ufsm/brunodea/tcc/event/EventItem.java
@@ -0,0 +1,22 @@
+package br.ufsm.brunodea.tcc.event;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.OverlayItem;
+
+public class EventItem extends OverlayItem {
+	
+	public enum EventType {
+		UNKNOWN,
+	}
+	
+	private EventType mType;
+	
+	public EventItem(GeoPoint pos, String title, String snippet, EventType type) {
+		super(pos, title, snippet);
+		mType = type;
+	}
+	
+	public EventType getType() {
+		return mType;
+	}
+}
