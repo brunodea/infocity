30a5db89c3b629536e39a7f0bae922a4329cc573
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-10 16:25:59 -0200

    classe para gerenciar EventOverlays.

---
+++ src/br/ufsm/brunodea/tcc/map/EventOverlayManager.java
@@ -0,0 +1,76 @@
+package br.ufsm.brunodea.tcc.map;
+
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import br.ufsm.brunodea.tcc.R;
+import br.ufsm.brunodea.tcc.event.EventItem;
+import br.ufsm.brunodea.tcc.event.EventItem.EventType;
+import br.ufsm.brunodea.tcc.map.EventsItemizedOverlay.BalloonType;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
+public class EventOverlayManager {
+	
+	private Context mContext;
+	private MapView mMapView;
+	private List<Overlay> mMapOverlays;
+	
+	private HashMap<EventType, EventsItemizedOverlay> sEIOMap;
+	
+	public EventOverlayManager(Context context, MapView mapview, 
+			List<Overlay> overlays) {
+		mContext = context;
+		mMapView = mapview;
+		sEIOMap = new HashMap<EventType, EventsItemizedOverlay>();
+		mMapOverlays = overlays;
+	}
+	
+	public EventsItemizedOverlay getEventOverlay(EventType type) {
+		EventsItemizedOverlay eio = null;
+		if(sEIOMap.containsKey(type)) {
+			eio = sEIOMap.get(type);
+		} else {
+			BalloonType balloon_type = null;
+			switch(type) {
+			case ADD:
+				balloon_type = BalloonType.ADD;
+				break;
+			default:
+				balloon_type = BalloonType.INFO;
+				break;
+			}
+			eio = new EventsItemizedOverlay(mContext, getEventTypeMarker(type),
+					mMapView, balloon_type);
+			sEIOMap.put(type, eio);
+			mMapOverlays.add(eio);
+		}
+		
+		return eio;
+	}
+	
+	public void addEventItem(EventItem item) {
+		getEventOverlay(item.getType()).addEventItem(item);
+	}
+	public void removeEventItem(EventItem item) {
+		getEventOverlay(item.getType()).removeEventItem(item);
+	}
+
+	public Drawable getEventTypeMarker(EventType type) {
+		Drawable marker = null;
+
+		switch(type) {
+		case UNKNOWN:
+			marker = mContext.getResources().getDrawable(R.drawable.ic_launcher);
+			break;
+		default:
+			marker = mContext.getResources().getDrawable(R.drawable.ic_launcher);
+			break;
+		}
+		
+		return marker;
+	}
+}
