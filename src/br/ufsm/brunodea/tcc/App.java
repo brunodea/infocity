cdba4ff86dd751e991a352dedcf1fdac2353079f
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-10 16:29:31 -0200

    Manager para EventsOverlay na classe App.

--- src/br/ufsm/brunodea/tcc/App.java
+++ src/br/ufsm/brunodea/tcc/App.java
@@ -1,14 +1,32 @@
 package br.ufsm.brunodea.tcc;
 
+import java.util.List;
+
 import android.app.Application;
+import android.content.Context;
+import br.ufsm.brunodea.tcc.map.EventOverlayManager;
+
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
 
 public class App extends Application {
 	private static App sInstance = null;
-	public App() {
-		sInstance = this;
+
+	private EventOverlayManager mEOManager;
+		
+	public void initEventOverlayManager(Context c, MapView mv, List<Overlay> overlays) {
+		mEOManager = new EventOverlayManager(c, mv, overlays);
+	}
+	
+	public EventOverlayManager getEventOverlayManager() {
+		return mEOManager;
 	}
 	
 	public static App instance() {
+		if(sInstance == null) {
+			sInstance = new App();
+		}
+		
 		return sInstance;
 	}
 }
