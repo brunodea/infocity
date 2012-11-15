2450c569172d57d55e7722ee5e60b3d3d5468bd2
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-15 15:30:12 -0200

    método abstrato para Model getListNameValuePair().

--- src/br/ufsm/brunodea/tcc/model/Model.java
+++ src/br/ufsm/brunodea/tcc/model/Model.java
@@ -1,5 +1,8 @@
 package br.ufsm.brunodea.tcc.model;
 
+import java.util.List;
+
+import org.apache.http.NameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -13,4 +16,5 @@ public abstract class Model extends OverlayItem {
 	}
 
 	public abstract JSONObject toJSON() throws JSONException;
+	public abstract List<NameValuePair> getListNameValuePair() throws JSONException;
 }
