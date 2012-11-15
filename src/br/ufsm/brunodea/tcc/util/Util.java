7ef02a004856e68d84e7e2174c932b5b04d08591
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-15 15:30:54 -0200

    método estático Util.toJSON() que pega um objeto e passa para JSON através do Gson.

--- src/br/ufsm/brunodea/tcc/util/Util.java
+++ src/br/ufsm/brunodea/tcc/util/Util.java
@@ -1,6 +1,18 @@
 package br.ufsm.brunodea.tcc.util;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
 
 public class Util {
 	public static String URL = "http://192.168.1.5:8000/events/";
+	
+	public static JSONObject toJSON(String modelname, Object obj) throws JSONException {
+		String json = new Gson().toJson(obj);
+		String root = "{'pk':null,'model':'events."+modelname+"','fields':"+json+"}";
+		
+		return new JSONObject(root);
+	}
 }
