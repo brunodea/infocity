4e2619407ae36399a0540316de186d5bfb344245
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-22 20:55:33 -0200

    preferências para mostrar localização do usuário no mapa e bússula.

--- src/br/ufsm/brunodea/tcc/util/InfoCityPreferenceActivity.java
+++ src/br/ufsm/brunodea/tcc/util/InfoCityPreferenceActivity.java
@@ -1,6 +1,7 @@
 package br.ufsm.brunodea.tcc.util;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -9,28 +10,87 @@ import android.view.Window;
 import android.widget.Toast;
 import br.ufsm.brunodea.tcc.R;
 
-public class InfoCityPreferenceActivity extends PreferenceActivity {
+public class InfoCityPreferenceActivity extends PreferenceActivity 
+										implements OnPreferenceChangeListener{
+	
+	private EditTextPreference mEventRadius;
+	private EditTextPreference mServerIP;
+	private EditTextPreference mServerPort;
+	
+	private CheckBoxPreference mEnableMyLocation;
+	private CheckBoxPreference mEnableCompass;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
         
+        setResult(0);
         addPreferencesFromResource(R.xml.preferences);
-        EditTextPreference event_radius = (EditTextPreference) getPreferenceScreen().findPreference("event_radius");
-        event_radius.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				boolean ok = true;
-				if(newValue.toString().equals("")) {
-					ok = false;
-					Toast.makeText(InfoCityPreferenceActivity.this, 
-							getResources().getString(R.string.not_empty), Toast.LENGTH_SHORT).show();
-				}
-				return ok;
-			}
-		});
+        
+        mEventRadius = (EditTextPreference)
+        		getPreferenceScreen().findPreference("event_radius");
+        mServerIP = (EditTextPreference)
+        		getPreferenceScreen().findPreference("server_ip");
+        mServerPort = (EditTextPreference)
+        		getPreferenceScreen().findPreference("server_port");
+
+        mEnableMyLocation = (CheckBoxPreference)
+        		getPreferenceScreen().findPreference("enable_mylocation");
+        mEnableCompass = (CheckBoxPreference)
+        		getPreferenceScreen().findPreference("enable_compass");
+        
+        mEventRadius.setSummary(InfoCityPreferences.getEventMaxRadius(this) + "m");
+        mServerIP.setSummary(InfoCityPreferences.getServerIP(this));
+        mServerPort.setSummary(InfoCityPreferences.getServerPort(this)+"");
+        
+        mEventRadius.setOnPreferenceChangeListener(this);
+        mServerIP.setOnPreferenceChangeListener(this);
+        mServerPort.setOnPreferenceChangeListener(this);
+        
+        mEnableCompass.setOnPreferenceChangeListener(this);
+        mEnableMyLocation.setOnPreferenceChangeListener(this);
     }
     
-    
+    private void showToastNotEmpty() {
+		Toast.makeText(InfoCityPreferenceActivity.this, 
+				getResources().getString(R.string.not_empty), Toast.LENGTH_SHORT).show();
+    }
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		boolean ok = false;
+		if(newValue.toString().equals("")) {
+			showToastNotEmpty();
+		} else {
+			if(preference == mEventRadius) {
+				ok = true;
+				setResult(1);
+			} else if(preference == mServerIP) {
+				String ip = newValue.toString();
+				if(ip.matches("^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|" +
+						"2[0-4]\\d|[0-1]?\\d?\\d)){3}$")) {
+					ok = true;
+				} else {
+					Toast.makeText(InfoCityPreferenceActivity.this, 
+							getResources().getString(R.string.invalid_ip), 
+							Toast.LENGTH_SHORT).show();
+				}
+			} else if(preference == mServerPort) {
+				String port = newValue.toString();
+				if(port.matches("\\d\\d\\d\\d(\\d)?")) {
+					ok = true;
+				} else {
+					Toast.makeText(InfoCityPreferenceActivity.this, 
+							getResources().getString(R.string.invalid_port), 
+							Toast.LENGTH_SHORT).show();
+				}
+			} else if(preference == mEnableCompass || preference == mEnableMyLocation) {
+				ok = true;
+				setResult(2);
+			}
+		}
+		
+		return ok;
+	}
 }
--- src/br/ufsm/brunodea/tcc/util/InfoCityPreferences.java
+++ src/br/ufsm/brunodea/tcc/util/InfoCityPreferences.java
@@ -17,9 +17,13 @@ public class InfoCityPreferences {
 			      "br.ufsm.brunodea.tcc.InfoCity", Context.MODE_PRIVATE);
 		return prefs.getString(key, _default);
 	}
-	private static float getFloatDefaultPreference(Context c, String key, float _default) {
+	private static String getStringDefaultPreference(Context c, String key, String _default) {
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(c);
-		return prefs.getFloat(key, _default);
+		return prefs.getString(key, _default);
+	}
+	private static boolean getBooleanDefaultPreference(Context c, String key, boolean _default) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(c);
+		return prefs.getBoolean(key, _default);
 	}
 	
 	public static void setAloharUID(Context c, String uid) {
@@ -30,6 +34,24 @@ public class InfoCityPreferences {
 	}
 	
 	public static float getEventMaxRadius(Context c) {
-		return getFloatDefaultPreference(c, "event_radius", 50000f);
+		return Float.parseFloat(getStringDefaultPreference(c, "event_radius", "50000"));
+	}
+	
+	public static int getServerPort(Context c) {
+		return Integer.parseInt(getStringDefaultPreference(c, "server_port",  "8000"));
+	}
+	public static String getServerIP(Context c) {
+		return getStringDefaultPreference(c, "server_ip",  "192.168.1.5");
+	}
+	
+	public static String getServerBaseURI(Context c) {
+		return "http://"+getServerIP(c)+":"+getServerPort(c)+"/events/";
+	}
+	
+	public static boolean shouldEnableMyLocation(Context c) {
+		return getBooleanDefaultPreference(c, "enable_mylocation", true);
+	}
+	public static boolean shouldEnableCompass(Context c) {
+		return getBooleanDefaultPreference(c, "enable_compass", false);
 	}
 }
