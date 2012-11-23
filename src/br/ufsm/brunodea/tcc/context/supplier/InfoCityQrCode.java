5a1aac88f8ca6aade1467f3f2982a64b6072b0c5
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-23 11:58:43 -0200

    adicionei ContextSupplier para o QrCode.

--- src/br/ufsm/brunodea/tcc/context/supplier/InfoCityQrCode.java
+++ src/br/ufsm/brunodea/tcc/context/supplier/InfoCityQrCode.java
@@ -1,13 +1,43 @@
 package br.ufsm.brunodea.tcc.context.supplier;
 
+import android.app.Activity;
 import android.os.Handler;
+import br.ufsm.brunodea.tcc.R;
 import br.ufsm.brunodea.tcc.context.ContextData;
 import br.ufsm.brunodea.tcc.context.ContextSupplier;
 
-public class InfoCityQrCode implements ContextSupplier {
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+public class InfoCityQrCode implements ContextSupplier {
+	private IntentIntegrator mZxingIntentIntegrator;
+	private ContextData mContextData;
+	
+	public InfoCityQrCode(Activity parent) {
+		mZxingIntentIntegrator = new IntentIntegrator(parent);
+		mZxingIntentIntegrator.setTitleByID(R.string.install_barcodescanner);
+		mZxingIntentIntegrator.setMessageByID(R.string.requires_barcodescanner);
+		
+		mContextData = new ContextData();
+	}
+	
+	public void finishedScan(IntentResult intentresult) {
+		String qrcode_json = intentresult.getContents();
+		if(qrcode_json != null && !qrcode_json.equals("")) {
+			try {
+				Gson gson = new Gson();
+				mContextData = gson.fromJson(qrcode_json, ContextData.class);
+			} catch(JsonSyntaxException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	@Override
 	public void init() {
+		mZxingIntentIntegrator.initiateScan();
 	}
 
 	@Override
@@ -16,7 +46,7 @@ public class InfoCityQrCode implements ContextSupplier {
 
 	@Override
 	public ContextData getContextData() {
-		return null;
+		return mContextData;
 	}
 	@Override
 	public void setHandler(Handler handler) {
